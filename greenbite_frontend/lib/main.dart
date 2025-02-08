dbbdff18ffdcd0fac1785ae418cf926bb822f8e4
Amiru Ushan <152049636+amiruushan@users.noreply.github.com>
2025-02-08 19:37:16 +0530

    save

--- greenbite_frontend/lib/main.dart
+++ greenbite_frontend/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "GreenBite",
+      title: "GreenddBite",
       theme: ThemeData(
         primarySwatch: Colors.green,
         useMaterial3: true,
