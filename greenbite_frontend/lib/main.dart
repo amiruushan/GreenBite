926ee953505b131c1a48873966344f0cb8a2544c
Amiru Ushan <152049636+amiruushan@users.noreply.github.com>
2025-02-10 14:56:55 +0530

    edit splash screen to navigate to the home page

--- greenbite_frontend/lib/main.dart
+++ greenbite_frontend/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:greenbite_frontend/screens/home_page/home_page.dart';
+import 'package:greenbite_frontend/screens/splash_screen/splash_screen.dart';
 import 'package:greenbite_frontend/screens/user_profile/user_profile_screen.dart';
 import 'screens/user_profile/edit_information.dart';
 
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
         useMaterial3: true,
       ),
-      home: HomePage(),
+      home: SplashScreen(),
     );
   }
 }
