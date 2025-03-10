090a177e2f9e6d626fb00b559c60d35f0e21afad
BK-Ashcharya-Douglas <ashbuisnessdouglas@gmail.com>
2025-03-11 00:04:08 +0530

    Updated Splash screen
    Updated the splash screen to accommodate the onboarding screen

--- greenbite_frontend/lib/screens/splash_screen/splash_screen.dart
+++ greenbite_frontend/lib/screens/splash_screen/splash_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:greenbite_frontend/screens/home_page/home_page.dart';
-import 'package:greenbite_frontend/screens/login/login_screen.dart';
+import 'package:greenbite_frontend/screens/onboarding/onboarding.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -16,7 +15,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
     // Navigate to login page with fade animation after 3 seconds
     Future.delayed(const Duration(seconds: 3), () {
-      Navigator.of(context).pushReplacement(_fadeRoute(LoginScreen()));
+      Navigator.of(context).pushReplacement(_fadeRoute(OnboardingScreen()));
     });
   }
 
